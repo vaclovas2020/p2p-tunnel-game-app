03f170bf5c0ca9013666ebf83ecbc821232e4a31
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2025-04-04 23:38:22 +0300

    remove unused assets and css

--- main.go
+++ main.go
@@ -17,15 +17,15 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "p2p-tunnel-game-app",
+		Title:  "P2P Tunnel Game",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: options.NewRGB(255, 255, 255),
 		OnStartup:        app.startup,
-		Bind: []interface{}{
+		Bind: []any{
 			app,
 		},
 	})
